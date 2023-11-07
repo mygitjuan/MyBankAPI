c5e2301e6437c79aade24ae8ce17955a5b553c6e
myGitJuan <jarellano.aguirre@gmail.com>
2023-11-07 18:05:55 +0100

    Limpieza main

---
+++ src/main/java/com/dxc/mypersonalbankapi/persistencia/IClientesRepo.java
@@ -0,0 +1,18 @@
+package com.dxc.mypersonalbankapi.persistencia;
+
+import com.dxc.mypersonalbankapi.modelos.clientes.Cliente;
+
+import java.util.List;
+
+public interface IClientesRepo {
+    public List<Cliente> getAll();
+
+    public Cliente getClientById(Integer id) throws Exception;
+
+    public Cliente addClient(Cliente cliente) throws Exception;
+
+    public boolean deleteClient(Cliente cliente) throws Exception;
+
+    public Cliente updateClient(Cliente cliente) throws Exception;
+
+}
