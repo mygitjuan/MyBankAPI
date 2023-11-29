bd2852dd865e087c4ce9010a61278aad991cf2cd
myGitJuan <jarellano.aguirre@gmail.com>
2023-11-29 10:43:39 +0100

    test cuentas añadir

---
+++ src/test/java/com/dxc/mypersonalbankapi/controladores/CuentasControllerTest.java
@@ -0,0 +1,28 @@
+package com.dxc.mypersonalbankapi.controladores;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentasControllerTest {
+
+    @Test
+    void mostrarLista() {
+    }
+
+    @Test
+    void mostrarDetalle() {
+    }
+
+    @Test
+    void eliminar() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void actualizar() {
+    }
+}
