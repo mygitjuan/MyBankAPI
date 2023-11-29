d5f0adea6bbf39989d36d60534f8c608ef1f4bdb
abelenl <anabelen.llorente@dxc.com>
2023-11-29 11:27:10 +0100

    Modificar ControllerConfig

--- src/main/java/com/dxc/mypersonalbankapi/config/ControllerConfig.java
+++ src/main/java/com/dxc/mypersonalbankapi/config/ControllerConfig.java
@@ -1,13 +1,27 @@
 package com.dxc.mypersonalbankapi.config;
 
+import com.dxc.mypersonalbankapi.controladores.ClientesController;
+import com.dxc.mypersonalbankapi.persistencia.IClientesRepo;
+import com.dxc.mypersonalbankapi.persistencia.ICuentasRepo;
+import com.dxc.mypersonalbankapi.persistencia.IPrestamosRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class ControllerConfig {
      @Autowired
+     private IClientesRepo clientesControl;
 
+     @Autowired
+     private static ICuentasRepo cuentasControl;
+     @Autowired
+     private static IPrestamosRepo prestamosControl;
+
+     @Bean
+     public ClientesController MostrarLista() {
+
+        return (ClientesController) clientesControl;
+     }
 }
 
